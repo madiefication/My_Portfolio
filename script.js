623a15c277fdb9b551e853468093527cde8f55f3
Madiha Fathima <104912488+madiefication@users.noreply.github.com>
2025-03-23 16:08:25 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,20 @@
 // Add animations on scroll
 document.addEventListener("DOMContentLoaded", function () {
   const sections = document.querySelectorAll("section");
+
+  const observer = new IntersectionObserver(
+    (entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add("animate__animated", "animate__fadeIn");
+          observer.unobserve(entry.target);
+        }
+      });
+    },
+    { threshold: 0.1 }
+  );
+
   sections.forEach((section) => {
-    section.classList.add("animate__animated", "animate__fadeIn");
+    observer.observe(section);
   });
 });
